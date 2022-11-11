1855625a8e19bc72213d8a401fcf76bec7753e60
Kehinde Malagu <Kehindemalagu@gmail.com>
2022-11-11 14:18:52 +0100

    Day 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,18 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
+    screens: {
+      'sm': '480px',
+      // => @media (min-width: 480px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '990px',
+      // => @media (min-width: 990px) { ... }
+    },
+
+
     extend: {
       fontFamily: {
         outfit: "outfit , sans-serif"
@@ -12,6 +24,12 @@ module.exports = {
         'greydark2': '#afa492',
         'brown2': '#d48d78',
         'brown3': '#a06857',
+      },
+
+      borderRadius:{
+        '4xl': '2rem',
+        '5xl': '2.5rem',
+        '6xl': '3rem'
       }
     },
   },
