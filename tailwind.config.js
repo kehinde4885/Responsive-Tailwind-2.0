097d66db8b9a898d22cb5df50e913d101454577d
Kehinde Malagu <Kehindemalagu@gmail.com>
2022-11-15 10:14:09 +0100

    Day 3 #Added Form Tailwind Plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,8 +12,6 @@ module.exports = {
       'lg': '990px',
       // => @media (min-width: 990px) { ... }
     },
-
-
     extend: {
       fontFamily: {
         outfit: "outfit , sans-serif"
@@ -26,9 +24,16 @@ module.exports = {
         'info-4': "url('./images/derma-4.png')",
         'info-5': "url('./images/derma-5.png')",
         'info-6': "url('./images/derma-6.png')",
+        'blog-1': "url('./images/Homepage-article-1.jpg')",
+        'blog-2': "url('./images/Homepage-article-2.jpg')",
+        'blog-3': "url('./images/Homepage-article-3.jpg')",
+        'blog-4': "url('./images/Homepage-article-4.jpg')",
+        'blog-5': "url('./images/Homepage-article-5.jpg')",
+        'footer': "url('./images/footer-img.jpg')",      
       },
 
       colors : {
+
         'greylight': '#f0ede8',
         'greydark': '#ddd8d1',
         'greydark2': '#afa492',
@@ -43,5 +48,7 @@ module.exports = {
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms')
+  ],
 }
